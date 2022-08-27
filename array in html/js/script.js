90106873fdd96085e4c66aea970b2d66a5c8b926
Muthurajchella- <muthuraj.chella@gmail.com>
2022-08-27 10:08:46 +0530

    js-practice

---
+++ array in html/js/script.js
@@ -0,0 +1,37 @@
+/*ARRAY EVERY and SOME method*/
+
+// const queue_by_age= [24, 30, 45];
+// let isAllow = queue_by_age.every(function(value, index, array){
+//     return value>=18;
+// });
+// document.write("isAllow: ", isAllow);
+// console.log("value :", isAllow);
+
+// const queue_by_age= [5, 15, 18];
+// let isAllow = queue_by_age.some(function(value, index, array){
+//     return value>=18;
+// });
+// document.write("isAllow: ", isAllow);
+// console.log("value :", isAllow);
+
+// const oddnum= [1, 3, 5, 61, 63,];
+
+// let odd=oddnum.every(function(value, index){
+//     return value % 2;
+// });
+// document.write("value :", odd); 
+// console.log("value :", odd);
+
+
+// const evennum= [8, 4, 2,];
+
+// let even=evennum.every(function(value, index){
+//     return value % 2===0;
+// });
+// document.write(even);
+// console.log("value :", even);
+let num = [];
+num.concat
+
+
+
---
+++ array/js/script.js
@@ -0,0 +1,16 @@
+var fruits= ["banana" , "mango", "pappaya",];
+document.write(fruits);
+/*a meaning of above array to consiter empty[no value]*/
+
+let favcolor= ["red", "white", "yellow", "blue",];
+document.write(favcolor);
+/*a meaning of above array to consiter all value*/
+
+
+let favnum= ["one", "two", "three", "four"];
+document.write(favnum[0]);
+/*a meaning of above array to consiter first value*/
+
+var 
+
+
