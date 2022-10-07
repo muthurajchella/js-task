6065333070e893ed45917e9d32c9aa97d27b8710
Muthurajchella- <muthuraj.chella@gmail.com>
2022-10-07 16:20:26 +0530

    conditions

---
+++ use-conditions/js/style.js
@@ -0,0 +1,24 @@
+let num1 = parseInt(prompt("give first num:"));
+let num2 = parseInt(prompt("give second num:"));
+let oprator = parseInt(prompt("give oprate:"));
+
+
+switch(oprator){
+    case +: 
+      
+}
+
+console.log(oprator)
+
+// let num1 = 5;
+// let num2 = 8;
+// let result;
+// switch (result){
+//     case 1:
+//         reuslt = num1 + num2;
+//         break;
+//     default:
+//         reuslt = "no added"
+// }
+
+// console.log(result)
