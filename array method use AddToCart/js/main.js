2962233b3f422f9a625a13a854cad96e08014cfd
Muthurajchella- <muthuraj.chella@gmail.com>
2022-10-09 20:13:53 +0530

    AddToCard

--- array method use AddToCart/js/main.js
+++ array method use AddToCart/js/main.js
@@ -1,5 +1,5 @@
 let moviesTickets = [
-    {TicketNo:1, MovieName: "KGF", TheaterName: "Sathyam Cinemas", ShowTime: "11pm",Rating: 4.8},
+    {TicketNo:1, MovieName: "KGF", TheaterName: "Sathyam Cinemas", ShowTime: "11pm",},
     {TicketNo:2, MovieName: "RRR", TheaterName: "Ram Cinemas", ShowTime: "11am",Rating: 5},
     {TicketNo:3, MovieName: "VIKRAM", TheaterName: "Udhayam Cinemas", ShowTime: "2pm",Rating: 4.9},
     {TicketNo:4, MovieName: "LKG", TheaterName: "Escape Multiz", ShowTime: "5pm",Rating: 4.5},
@@ -7,29 +7,28 @@ let moviesTickets = [
     
 ]
 
+let yourTickets = [];
 
-let buyTickets = [];
+let getBtn1 = document.getElementById("btn1");
 
+function bookTicket(){
+    let book = parseInt(prompt("Enter You'r Ticket Number"))
+    let ticket = moviesTickets.find(function(item, index){
+        return item.TicketNo === book;
+    })
+    if(ticket){
+        let tickets = yourTickets.map(function(item, index){
+            if(item.TicketNo == book){
+                item.qty = item.qty+1;
+                return item; 
 
-let clickToAvaliable = document.getElementById("btn1");
-
-
-function checkTicketNo(){
-    let a = parseInt(prompt("Give TicketNo:"));
-   buyTicket = moviesTickets.find(find => find.TicketNo === a)
-   var Qty = 1
-   if(buyTicket){
-    let easy = buyTickets.some(some => some.TicketNo === a);
-    if(easy){
-        buyTickets.find(find => find.Qty++);
-    }else{
-        buyTickets.push({...buyTicket,Qty:1});
+            }
+        })
     }
-   }
-  
-console.log(buyTickets);
-
+    ticket.qty = 1;
+    yourTickets.push(ticket);
+    console.log(yourTickets)
 }
-clickToAvaliable.addEventListener("click", checkTicketNo);
 
+getBtn1.addEventListener("click", bookTicket);
 
