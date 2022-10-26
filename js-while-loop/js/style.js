861dcb57cd0fe3a78c94c2de3e2d6e7089ec0a5d
Muthurajchella- <muthuraj.chella@gmail.com>
2022-10-26 12:49:53 +0530

    Sum of the input

--- js-while-loop/js/style.js
+++ js-while-loop/js/style.js
@@ -9,14 +9,37 @@
 // }
 
 
-let i = 1;
-for(i=0; i<=100; i++){
-    if(i%7===0){
-        var a = i;
-        if(a%10!==1){
-            console.log(a)
-        }
-    }
+// let i = 1;
+// for(i=0; i<=100; i++){
+//     if(i%7===0){
+//         var a = i;
+//         if(a%10!==1){
+//             console.log(a)
+//         }
+//     }
    
+// }
+
+
+// let a = 124;
+// let result = 0;
+// while(a>0){
+//     let rem = a%10;
+//     a = (a-rem)/10;
+//     result = (result*10)+rem;
+// }
+// console.log(result);
+
+
+let num = 1978;
+let result = 0
+while(num>0){
+    let rem = num%10;
+    num = (num-rem)/10;
+    console.log(rem);
+    console.log(num);
+    result +=rem
+    
 }
 
+console.log("result", result);
